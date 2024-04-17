4993cb2f3c13f6597de940fb8a95b8bf449eb794
Maryna Akil <133876007+MarynaLapa@users.noreply.github.com>
2024-04-17 13:47:57 +0300

    Маршрутизація та рендінг сторінки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import HomePage from "pages/HomePage/HomePage";
+
+
+const Catalog = lazy(() => import('pages/Catalog/Catalog'))
+const Favorites = lazy(()=>import('pages/Favorites/Favorites'))
+// const Features = lazy(() => import('./Features/Features'))
+// const Reviews = lazy(() => import('./Reviews/Reviews'))
+
 export const App = () => {
+  //Потрібно зробити сторінку фаворіт, а на ній модалку в якій вже є відміності.??
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Suspense fallback={<>Loading...</>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="favorites" element={<Favorites />}>
+            {/* <Route path="features" element={<Features />} />
+            <Route path="reviews" element={<Reviews />} /> */}
+          </Route>
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+        <BrowserRouter basename="camper-test-task">
+          <App />
+        </BrowserRouter>
+      {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>
 );

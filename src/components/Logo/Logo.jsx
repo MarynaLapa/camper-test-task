8c9dc5c8ad7a94480e802dd09f633c3a2275c3f3
Maryna Akil <133876007+MarynaLapa@users.noreply.github.com>
2024-04-26 14:27:54 +0300

    svg

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+   <svg viewBox="0 0 1320 300">
+        <text x="50%" y="50%" dy=".35em" textAnchor="middle">
+            Camper in Ukraine
+        </text>
+    </svg>
+  )
+}
+
+export default Logo
+
+
+
